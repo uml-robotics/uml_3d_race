04dfe0885e2adf75aa78b34bbc97b339d1e84101
Kevin Orsula <Kevin_Orsula@student.uml.edu>
2020-07-02 15:08:12 -0400

    korusla after level4

--- src/mover.cpp
+++ src/mover.cpp
@@ -1,71 +1,52 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "sensor_msgs/LaserScan.h"
-#include "geometry_msgs/Twist.h"
+#include "geometry_msgs/Point.h"
+#include "uml_3d_race/Goal.h"
+#include "move_base_msgs/MoveBaseAction.h"
+#include "actionlib/client/simple_action_client.h"
 
-sensor_msgs::LaserScan scan;
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+MoveBaseClient *ac ;
 
-void scanCallback(const sensor_msgs::LaserScan info){
-  // Save scan information
-  scan = info;
+void goalCallback( uml_3d_race::Goal goal){
+
+    //wait for the action server to come up
+    while(!ac->waitForServer(ros::Duration(5.0))){
+        ROS_INFO("Waiting for the move_base action server to come up");
+    }
+
+       move_base_msgs::MoveBaseGoal cmd;
+
+       cmd.target_pose.header.frame_id = "map";
+       cmd.target_pose.header.stamp = ros::Time::now();
+
+       cmd.target_pose.pose.position.x = goal.x;
+       cmd.target_pose.pose.position.y = goal.y;
+       cmd.target_pose.pose.orientation.w = 1.0;
+
+       ROS_INFO("Sending goal");
+       ac->sendGoal(cmd);
+
+       ac->waitForResult();
+
 }
 
-int main (int argc, char **argv){
-// Setup ros node and NodeHandle
-ros::init(argc,argv,"mover_node");
-ros::NodeHandle n;
+int main (int argc, char **argv) {
+    // Setup ros node and NodeHandle
+    ros::init(argc,argv,"mover_node");
+    ros::NodeHandle n;
 
-// Setup subscriber to poll the laser scan topic being published by our robot
-ros::Subscriber sub = n.subscribe("/pioneer/frontscan_filtered",1000,scanCallback);
-// Setup publisher object to publish movement commands to our robot
-ros::Publisher twist_pub = n.advertise<geometry_msgs::Twist>("/pioneer/cmd_vel",100);
+    ros::Subscriber sub = n.subscribe("/goal",1000,goalCallback);
+    ac = new MoveBaseClient("/move_base", true);
 
-// Declare working variables
-geometry_msgs::Twist command;
-float speed, angle, left, right, diff, threshold;
+    ros::Rate loop_rate(20);
 
-// Set loop rate
-ros::Rate loop_rate(20);
-// Main control loop
-while (ros::ok()){
-  // Poll subscribed laser topic
-  ros::spinOnce();
-
-  if (!scan.ranges.empty()){
-    // scan.ranges is a vector filled with 240 distance measurements (in meters).
-    // The blue rays coming off of the robot represent these values.
-    // The total scan is 240 degrees centered in front of the robot, meaning
-    // scan.ranges[119] is straight ahead, scan.ranges[0] is all the way to the
-    // left, and scan.ranges[239] is all the way to the right.
-
-    // Set Variables
-    speed = 0.35f; // Speed to be published
-    angle = 0.40f; // Angle to be published
-
-    //ROS_INFO("DISTANCE AHEAD: %.2f",scan.ranges[119]);
-
-    left = scan.ranges[74]; // Range measurement 45 Degrees left of center
-    right = scan.ranges[164];// Range measurement 45 Degrees right of center
-    diff = right-left;
-    //ROS_INFO("LEFT: %.2f | RIGHT: %.2f | DIFF: %.2f",left, right, diff);
-
-    // Setting a threshold here will reduce wiggling movement (try removing it)
-    threshold = 0.25;
-    if( abs(diff) < threshold)
-      angle = 0.0;
-    else if (diff < 0.0)
-      angle = -angle;
-
-    // Publish Commands
-    // linear.x is forward/backward movement. Positive values for forward.
-    command.linear.x  = speed;
-    // angular.z is turning movement. Positive values turn left, negative turns right.
-    command.angular.z = angle;
-    //ROS_INFO(" PUBLISHING Speed:%.2f | Angle:%.2f\n",speed,angle);
-    twist_pub.publish(command);
-  }
-  loop_rate.sleep();
-}
+    while (ros::ok()) {
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
 
 return 0;
+
 }
