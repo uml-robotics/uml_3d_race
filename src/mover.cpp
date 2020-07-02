3f27a0e59e6be46b9990eacb488a78e2369dd50f
Charlie K <charlie-kenny@hotmail.com>
2020-07-02 14:44:48 -0400

    First commit to the remote repository.  So far, mover.cpp has been changed to publish goals for the navigation stack instead of publishing direct velocity commands.  Also, instead of subscribing to the laser scan, the node is subscribing to the custom goal msg to retrieve the goal coordinates. Next, in the amcl launch file, the angle argument was changed to accept radians in order to be consistent with the yaw goal args.  Finally, an additional navigation launch file was added for doing navigation without a predefined global map.

--- src/mover.cpp
+++ src/mover.cpp
@@ -1,71 +1,50 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "sensor_msgs/LaserScan.h"
-#include "geometry_msgs/Twist.h"
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
 
-sensor_msgs::LaserScan scan;
+//Goal is a custom message type defined in uml_3d_race/msg/Goal.msg
+#include <uml_3d_race/Goal.h>
 
-void scanCallback(const sensor_msgs::LaserScan info){
-  // Save scan information
-  scan = info;
+typedef actionlib::SimpleActionClient <move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+MoveBaseClient *ac;
+
+void goalCallback(uml_3d_race::Goal goalMsg) {
+    move_base_msgs::MoveBaseGoal goal;
+
+    //translate Goal Message to a MoveBaseGoal
+    goal.target_pose.header.frame_id = "map";
+    goal.target_pose.header.stamp = ros::Time::now();
+    goal.target_pose.pose.position.x = goalMsg.x;
+    goal.target_pose.pose.position.y = goalMsg.y;
+    goal.target_pose.pose.orientation.w = 1.0;
+
+    //Send the goal to the navigation action client
+    ac->sendGoal(goal);
+
+    //Wait for the robot to finish moving
+    ac->waitForResult();
+
+    return;
 }
 
-int main (int argc, char **argv){
-// Setup ros node and NodeHandle
-ros::init(argc,argv,"mover_node");
-ros::NodeHandle n;
+int main(int argc, char **argv) {
+    // Setup ros node and NodeHandle
+    ros::init(argc, argv, "mover_node");
+    ros::NodeHandle n;
 
-// Setup subscriber to poll the laser scan topic being published by our robot
-ros::Subscriber sub = n.subscribe("/pioneer/frontscan_filtered",1000,scanCallback);
-// Setup publisher object to publish movement commands to our robot
-ros::Publisher twist_pub = n.advertise<geometry_msgs::Twist>("/pioneer/cmd_vel",100);
+    ac = new MoveBaseClient("move_base", true);
 
-// Declare working variables
-geometry_msgs::Twist command;
-float speed, angle, left, right, diff, threshold;
+    //wait for the action server to come up
+    while (!ac->waitForServer(ros::Duration(5.0))) {
+        ROS_INFO("Waiting for the move_base action server to come up");
+    }
 
-// Set loop rate
-ros::Rate loop_rate(20);
-// Main control loop
-while (ros::ok()){
-  // Poll subscribed laser topic
-  ros::spinOnce();
+    //Subscribe to the goal topic
+    ros::Subscriber sub = n.subscribe("goal/", 1000, goalCallback);
 
-  if (!scan.ranges.empty()){
-    // scan.ranges is a vector filled with 240 distance measurements (in meters).
-    // The blue rays coming off of the robot represent these values.
-    // The total scan is 240 degrees centered in front of the robot, meaning
-    // scan.ranges[119] is straight ahead, scan.ranges[0] is all the way to the
-    // left, and scan.ranges[239] is all the way to the right.
+    ros::spin();
 
-    // Set Variables
-    speed = 0.35f; // Speed to be published
-    angle = 0.40f; // Angle to be published
-
-    //ROS_INFO("DISTANCE AHEAD: %.2f",scan.ranges[119]);
-
-    left = scan.ranges[74]; // Range measurement 45 Degrees left of center
-    right = scan.ranges[164];// Range measurement 45 Degrees right of center
-    diff = right-left;
-    //ROS_INFO("LEFT: %.2f | RIGHT: %.2f | DIFF: %.2f",left, right, diff);
-
-    // Setting a threshold here will reduce wiggling movement (try removing it)
-    threshold = 0.25;
-    if( abs(diff) < threshold)
-      angle = 0.0;
-    else if (diff < 0.0)
-      angle = -angle;
-
-    // Publish Commands
-    // linear.x is forward/backward movement. Positive values for forward.
-    command.linear.x  = speed;
-    // angular.z is turning movement. Positive values turn left, negative turns right.
-    command.angular.z = angle;
-    //ROS_INFO(" PUBLISHING Speed:%.2f | Angle:%.2f\n",speed,angle);
-    twist_pub.publish(command);
-  }
-  loop_rate.sleep();
-}
-
-return 0;
+    return 0;
 }
