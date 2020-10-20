f2a005900a6acdb3aa3fdf0a831c2e8a8e1b3680
Charlie Kenny <charlie-kenny@gotmail.com>
2020-10-20 01:52:34 -0400

    init changes

--- setup.sh
+++ setup.sh
@@ -11,15 +11,15 @@ echo "Grant permissions for Gazebo Textures folder:"
 sudo chmod 777 /usr/share/gazebo-7/media/materials/*
 echo
 echo "Copying Models..."
-echo "- SDF Pioneer 2 Wheel"; cp -r ./src/uml_3d_race/resources/models/sdf_robots/pioneer_2wd $destination
-echo "- SDF Pioneer 4 Wheel"; cp -r ./src/uml_3d_race/resources/models/sdf_robots/pioneer_4wd $destination
-echo "- Track Pieces"; cp -r ./src/uml_3d_race/resources/models/track_models/* $destination
-echo "- Obstacles"; cp -r ./src/uml_3d_race/resources/models/obstacles/* $destination
+echo "- SDF Pioneer 2 Wheel"; cp -r ./resources/models/sdf_robots/pioneer_2wd $destination
+echo "- SDF Pioneer 4 Wheel"; cp -r ./resources/models/sdf_robots/pioneer_4wd $destination
+echo "- Track Pieces"; cp -r ./resources/models/track_models/* $destination
+echo "- Obstacles"; cp -r ./resources/models/obstacles/* $destination
 echo "Done."
 echo
 echo "Copying Textures:"
-echo "- Caution Material Script"; cp ./src/uml_3d_race/resources/textures/caution_texture/scripts/caution.material $destination2/scripts
-echo "- Caution Material Images"; cp ./src/uml_3d_race/resources/textures/caution_texture/textures/* $destination2/textures
-echo "- Concrete Material Script"; cp ./src/uml_3d_race/resources/textures/concrete_texture/scripts/concrete.material $destination2/scripts
-echo "- Concrete Material Image"; cp ./src/uml_3d_race/resources/textures/concrete_texture/textures/* $destination2/textures
+echo "- Caution Material Script"; cp .resources/textures/caution_texture/scripts/caution.material $destination2/scripts
+echo "- Caution Material Images"; cp ./resources/textures/caution_texture/textures/* $destination2/textures
+echo "- Concrete Material Script"; cp ./resources/textures/concrete_texture/scripts/concrete.material $destination2/scripts
+echo "- Concrete Material Image"; cp ./resources/textures/concrete_texture/textures/* $destination2/textures
 echo "Done."
