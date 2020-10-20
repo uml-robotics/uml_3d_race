577183079e5f2be4aacc28815395790ba5560093
Charlie K <charlie-kenny@hotmail.com>
2020-10-20 10:36:47 -0400

    level launch rewrites and added opposite goal pub

--- src/goal_pub.cpp
+++ src/goal_pub.cpp
@@ -6,6 +6,7 @@
 #include <uml_3d_race/Goal.h>
 
 ros::Publisher goal_pub;
+ros::Publisher goal_pub_opposite;
 int counter;
 float goal_x;
 float goal_y;
@@ -16,6 +17,7 @@ float goal_tolerance;
 bool callback(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res)
 {
   uml_3d_race::Goal goal;
+  uml_3d_race::Goal goal_opposite;
 
   //Determines which goal needs to be published
   if(counter % 2 == 0)
@@ -24,6 +26,11 @@ bool callback(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res)
     goal.x = goal_x;                  
     goal.y = goal_y;
     goal.tolerance = goal_tolerance;
+
+    //Set the opposite goal to the original spawn coords
+    goal_opposite.x = spawn_x;                  
+    goal_opposite.y = spawn_y;
+    goal_opposite.tolerance = goal_tolerance;
   }
   else
   {
@@ -31,10 +38,18 @@ bool callback(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res)
     goal.x = spawn_x;                  
     goal.y = spawn_y;
     goal.tolerance = goal_tolerance;
+
+    //Set the opposite goal to the original goal coords
+    goal_opposite.x = goal_x;                  
+    goal_opposite.y = goal_y;
+    goal_opposite.tolerance = goal_tolerance;
   }
 
   //Publish the goal
   goal_pub.publish(goal);
+
+  //Publish the opposite goal
+  goal_pub_opposite.publish(goal_opposite);
 
   //Increase count to alternate goals
   counter++;
@@ -54,7 +69,6 @@ int main(int argc, char **argv){
   spawn_x = 0.0;
   spawn_y = 0.0;
   goal_tolerance = 0.9;
-  std::string topic = "goal";
 
   //Retrieve node parameters
   n.getParam(ros::this_node::getName()+"/goal_x",goal_x);
@@ -62,13 +76,15 @@ int main(int argc, char **argv){
   n.getParam(ros::this_node::getName()+"/spawn_x",spawn_x);
   n.getParam(ros::this_node::getName()+"/spawn_y",spawn_y);
   n.getParam(ros::this_node::getName()+"/goal_tolerance",goal_tolerance);
-  n.getParam(ros::this_node::getName()+"/topic",topic);
 
   //Display node information on startup
-  ROS_INFO("GOAL_A | Point: (%.2f,%.2f)\tGOAL_B | Point: (%.2f,%.2f)\tTolerance: %.2f\tTopic: %s",goal_x,goal_y,spawn_x,spawn_y,goal_tolerance,topic.c_str());
+  ROS_INFO("GOAL_A | Point: (%.2f,%.2f)\tGOAL_B | Point: (%.2f,%.2f)\tTolerance: %.2f",goal_x,goal_y,spawn_x,spawn_y,goal_tolerance);
 
   //Create the publisher object
-  goal_pub = n.advertise<uml_3d_race::Goal>(topic, 10, false);
+  goal_pub = n.advertise<uml_3d_race::Goal>("goal", 10, false);
+
+  //Create the opposite publisher object, mainly used by obstacle bot
+  goal_pub_opposite = n.advertise<uml_3d_race::Goal>("goal_opposite", 10, false);
   
   //Create the service callback
   ros::ServiceServer goal_server = n.advertiseService("get_new_goal", callback);
